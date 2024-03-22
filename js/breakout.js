74407386ec5d2b5db06fa3dc4bc746c3925f4eb1
Emily Chevy <142541332+EmChevy@users.noreply.github.com>
2024-03-22 22:01:59 +0000

    Fri, Mar 22, 2024, 3:01 PM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -138,8 +138,34 @@ document.addEventListener('keydown', keyDown)
 document.addEventListener('keyup', keyUp)
 
 
+function moveBall() {
+    ball.x = ball.x + ball.dx
+    ball.y = ball.y + ball.dy
+
+    // wall collision (top)
+    if (ball.y + ball.size < 0) {
+        ball.dy = -1 * ball.dy
+    }
+
+    // wall collision (right)
+    if (ball.x + ball.size > canvas.width) {
+        ball.dx = -1 * ball.dx
+    }
+
+    // wall collision (bottom)
+    if (ball.y + ball.size  > canvas.height) {
+        ball.dy = -1 * ball.dy
+    }
+
+    // wall collision (left)
+    if (ball.x + ball.size < 0) {
+        ball.dx = -1 * ball.dx
+    }
+}
+
 // Update canvas drawing and animation
 function update() {
+    moveBall()
     movePaddle()
     draw()
     requestAnimationFrame(update)
