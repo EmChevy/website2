c06a9f449340f5f11308de8eae7f6af425f03fd5
Emily Chevy <142541332+EmChevy@users.noreply.github.com>
2024-04-02 21:50:21 +0000

    Tue, Apr 2, 2024, 2:50 PM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -170,7 +170,26 @@ function moveBall() {
     ) {
         ball.dy = -1 * ball.dy
     }
+
+    // Brick collision
+    bricks.forEach(column => {
+        column.forEach(brick => {
+            if (brick.visible) {
+                if (
+                    ball.x - ball.size > brick.x && // left brick side
+                    ball.x + ball.size < brick.x + brick.w && // right
+                    ball.y - ball.size < brick.y + brick.h // bottom
+                ){
+                    ball.dy = -1 * ball.dy
+                    brick.visible = false
+                }
+
+            }
+        })
+    })
 }
+
+
 
 // Update canvas drawing and animation
 function update() {
